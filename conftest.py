99933cc4b892b72345519234eb51fd28951dcd0a
ivan2007000 <41183245+ivan2007000@users.noreply.github.com>
2019-07-02 16:15:29 +0300

    Edit error

--- conftest.py
+++ conftest.py
@@ -2,36 +2,29 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default="chrome",
+    parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default="ru",
+    parser.addoption('--language', action='store', default='ru',
                      help="Choose language")
-
 
 @pytest.fixture(scope="function")
 def browser(request):
-    # read browser_name
+    language = request.config.getoption("language")
     browser_name = request.config.getoption("browser_name")
-
-    # read language
-    user_language = request.config.getoption("language")
-
     if browser_name == "chrome":
-        # set language
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-        browser = webdriver.Chrome(options=options)
         print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
-        # set language
-        fp = webdriver.FirefoxProfile()
-        fp.set_preference("intl.accept_languages", user_language)
-        browser = webdriver.Firefox(firefox_profile=fp)
         print("\nstart firefox browser for test..")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", language)
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
         print("Browser {} still is not implemented".format(browser_name))
+    browser.implicitly_wait(5)
     yield browser
     print("\nquit browser..")
     browser.quit()
